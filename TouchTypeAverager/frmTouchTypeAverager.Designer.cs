0214b356f1ec302b8e2bdb170d280b9c9072202d
mmackaynbcc <mary.mackay1993@gmail.com>
2021-02-01 13:57:08 -0400

    Add project files.

---
+++ TouchTypeAverager/frmTouchTypeAverager.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace TouchTypeAverager
+{
+    partial class frmTouchTypeAverager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWordCount = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRunningAvg = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstTests = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Touch Type Word Averager (5 scores)";
+            // 
+            // txtWordCount
+            // 
+            this.txtWordCount.Location = new System.Drawing.Point(39, 59);
+            this.txtWordCount.Name = "txtWordCount";
+            this.txtWordCount.Size = new System.Drawing.Size(185, 20);
+            this.txtWordCount.TabIndex = 1;
+            this.txtWordCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(230, 56);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(93, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Avg:";
+            // 
+            // lblRunningAvg
+            // 
+            this.lblRunningAvg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRunningAvg.Location = new System.Drawing.Point(83, 95);
+            this.lblRunningAvg.Name = "lblRunningAvg";
+            this.lblRunningAvg.Size = new System.Drawing.Size(100, 23);
+            this.lblRunningAvg.TabIndex = 4;
+            this.lblRunningAvg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "My Results";
+            // 
+            // lstTests
+            // 
+            this.lstTests.FormattingEnabled = true;
+            this.lstTests.Location = new System.Drawing.Point(39, 174);
+            this.lstTests.Name = "lstTests";
+            this.lstTests.Size = new System.Drawing.Size(284, 95);
+            this.lstTests.TabIndex = 6;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(39, 287);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(284, 30);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // frmTouchTypeAverager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 352);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstTests);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblRunningAvg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtWordCount);
+            this.Controls.Add(this.label1);
+            this.Name = "frmTouchTypeAverager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Touch Type Averager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWordCount;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRunningAvg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstTests;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
